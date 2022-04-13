d7b00923e0010a288dea2160ed62992ec154148b
52610_Shridhar <shridharkatkar009@gmail.com>
2022-04-13 17:31:14 +0530

    make it better

--- hospitalMgmtSystem/settings.py
+++ hospitalMgmtSystem/settings.py
@@ -29,7 +29,7 @@ DEBUG = False
 # local true
 # DEBUG = True
 
-ALLOWED_HOSTS = [ '.herokuapp.com' ,'127.0.0.1']
+ALLOWED_HOSTS = [ 'shridharkatkar.herokuapp.com' ,'127.0.0.1']
 
 
 # Application definition
